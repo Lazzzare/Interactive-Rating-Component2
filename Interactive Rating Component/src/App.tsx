eeaa751c75f5684743105e36157b745a88c903f0
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-03-31 13:57:35 +0400

    Update App.tsx

--- Interactive Rating Component/src/App.tsx
+++ Interactive Rating Component/src/App.tsx
@@ -5,7 +5,50 @@ type TPage = "choose" | "result";
 
 function App() {
   const [page, setPage] = useState<TPage>("choose");
-  return <div className="card">{page === "choose" && <h1></h1>}</div>;
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (rating: number) => {
+    setRating(rating);
+  };
+
+  const handleSubmit = () => {
+    setPage("result");
+  };
+
+  return (
+    <div className="card">
+      {page === "choose" && (
+        <>
+          <img src="../assets/icon-star.svg" alt="" className="star-icon" />
+
+          <h1 className="title">How did we do?</h1>
+          <p className="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+            sit suscipit sint molestiae reiciendis iure, libero iusto sed quod.
+          </p>
+
+          <div className="buttons">
+            {[1, 2, 3, 4, 5].map((button, i) => {
+              const isActive = button === rating;
+              return (
+                <div
+                  className={`button ${isActive ? "activeButton" : null}`}
+                  key={i}
+                  onClick={() => handleRating(button)}
+                >
+                  {button}
+                </div>
+              );
+            })}
+          </div>
+
+          <button className="submitButton" onClick={() => handleSubmit()}>
+            Submit
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
